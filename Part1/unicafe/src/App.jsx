1a986d9bd61fd1eea10c5a322d46c90496921d59
Donna Njoki <njerinjoki22@gmail.com>
2024-02-20 01:19:31 +0300

    unicafe

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import reactLogo from "./assets/react.svg";
+const statistics = (props) => {};
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // states for each feedback category and total count
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // update state and the total count for each button click
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  // calculate total feedback, average score and positive percentage
+  const totalFeedback = good + neutral + bad;
+  // note: Average Score = (Number of Good Responses * 1 + Number of Neutral Responses * 0 + Number of Bad Responses * -1) / Total Number of Responses
+  const averageScore = (good - bad) / totalFeedback;
+
+  const positivePercentage = Math.round((good / totalFeedback) * 100);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
 
-export default App
+      <h1>Statistics</h1>
+      <p>Good: {good} </p>
+      <p>Neutral: {neutral} </p>
+      <p> Bad: {bad} </p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Average Score: {averageScore}</p>
+      <p>positivePercentage: {positivePercentage}%</p>
+    </div>
+  );
+};
+export default App;
